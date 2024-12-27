27cb08dc2ac9845ea0b4b77d155ce895ddf7f7d2
Umaiir11 <iam.umairimran@gmail.com>
2024-12-27 10:04:36 +0500

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+// Dart imports:
+import 'dart:math' as math;
+
+/// Note that the userID needs to be globally unique,
+final String localUserID = math.Random().nextInt(10000).toString();
+
+const yourAppID = 2051298863;
+
+const yourAppSign = 'ceaa1c5bed4ff07f70eb7a54611df1f9d06e4aa13a3015a747ef598f709d5057';
+
+// const yourServerSecret = ;
